9638c01878411e74253ed744b5386aaa62fe2fc4
Dmytro <dim4ukt@gmail.com>
2024-11-06 11:08:57 +0100

    Initial commit

---
+++ Kth_Largest_Sum_in_a_Binary_Tree.py
@@ -0,0 +1,17 @@
+def removeSubfolders(folder):
+    folder = sorted(folder)
+    result = set()
+    for x in folder:
+        p = x.split("/")
+        parent_dir = ''
+        for x in p[1:]:
+            parent_dir += "/" + x
+            if parent_dir in result:
+                break
+            else:
+                result.add(parent_dir)
+
+
+    return list(result)
+
+print(removeSubfolders(["/a","/a/b","/c/d","/c/d/e","/c/f"])) # ["/a","/c/d","/c/f"]
---
+++ MaximumSwap.py
@@ -0,0 +1,31 @@
+num = int(input("Enter a number: "))
+
+def maximumSwap(num):
+    res = [int(x) for x in str(num)]
+    i = 0
+
+    for x in res:
+        while i<len(res):
+            if x > res[i]:
+                b = res[i]
+                x, res[i] = res[i], x
+            else:
+                pass
+
+            i+=1
+
+    print(res)
+
+
+
+
+
+
+
+
+    # maxIndex = res.index(max(res))
+    # res[maxIndex],res[0]=res[0],res[maxIndex]
+    # integer = int("".join([str(x) for x in res]))
+    # return integer
+# res[res.index(x)], res[res.index(y)] = res[res.index(y)], res[res.index(x)]
+maximumSwap(num)
